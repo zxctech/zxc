c286d8904deab921d5e52236ef9bbbf602b22973
Ronald Manzano <ron.zxc.tech@gmail.com>
2021-05-28 14:49:41 +1000

    update

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
-cd /home/ec2-user/server/src
-npm run build
-pm2 serve build 3000 --spa
+
+# navigate to app folder
+cd /app
+
+# initial startup by running react-script "start", name process "marketing"
+# --watch watches and restarts if files change
+pm2 start ./node_modules/react-scripts/scripts/start.js --name "zxcapp" --watch
+
+# auto restart server if shut down
 pm2 startup
+
+# freeze process list for automatic respawn
 pm2 save
+
+# restart all processes - necessary to do this again?
 pm2 restart all
